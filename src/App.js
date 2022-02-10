5f84ef10ea179a5471ba38de40170636213cc807
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-10 21:36:19 +0200

    added markup of Modal & Button

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import s from './App.css';
 import Searchbar from './components/Searchbar/Searchbar';
 // import Loader from './components/Loader/Loader';
-// import Button from './components/Button/Button';
-// import Modal from './components/Modal/Modal';
+import Button from './components/Button/Button';
+import Modal from './components/Modal/Modal';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 
 class App extends Component {
+  state = {
+    showModal: false
+  }
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal
+    }))
+  }
 
   render() {
+    const { showModal } = this.state;
     return (
       <div className={s.app} >
         <Searchbar />
         <ImageGallery />
-        {/* <Loader />
-        <Button />
-        <Modal /> */}
+        {/* <Loader /> */}
+        <Button toggleModal={this.toggleModal} />
+        {showModal && <Modal />}
       </div>
     );
   }
